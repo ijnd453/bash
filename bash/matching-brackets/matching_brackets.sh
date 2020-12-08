daad96859b2bc2a3b89be269aef4ca5668a968be
ijnd453 <ijnd@umich.edu>
2020-12-07 23:38:57 -0500

    completed matching_brackets

--- bash/matching-brackets/matching_brackets.sh
+++ bash/matching-brackets/matching_brackets.sh
@@ -1,6 +1,60 @@
 #!/usr/bin/env bash
 
 str=$1
-arr=( $1 )
-count=${#arr}
-echo $count
+var=true
+count=${#str}
+curly=0
+square=0
+round=0
+for(( i=0; i<count; i++)); do
+	chr="${str:$i:1}"
+	case $chr in
+
+		{)
+			let "curly++"
+		;;
+
+		[)
+			let "square++"
+		;;
+
+		\()
+			let "round++"
+		;;
+
+		})
+			if [[ $curly -gt "0" ]]; then
+				let "curly--"
+			else
+				var=false
+				break
+			fi
+		;;
+
+		])
+			if [[ $square -gt "0" ]]; then
+				let "square--"
+			else
+				var=false
+				break
+			fi
+		;;
+
+		\))
+			if [[ $round -gt "0" ]]; then
+				let "round--"
+			else
+				var=false
+				break
+			fi
+		;;
+
+		*)
+			continue
+		;;
+	esac
+done
+if [[ ( "$curly" -gt "0" ) || ( "$square" -gt "0" ) || ( "$round" -gt "0" ) ]]; then
+	var=false
+fi
+echo $var
