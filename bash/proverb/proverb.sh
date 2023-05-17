5524f44930603d407a387dbc4b05d29b4a0e3f55
ijnd453 <ijnd@umich.edu>
2023-05-17 19:23:37 -0400

    Delete bash directory

--- bash/proverb/proverb.sh
+++
@@ -1,8 +0,0 @@
-for((i=1; i <= $[$# - 1]; i++));
-do
-	j=$(($i+1))
-	echo "For want of a ${!i} the ${!j} was lost."
-done
-if (( $# >= 1 )); then
-	echo "And all for the want of a $1."
-fi
--- bash/two-fer/two_fer.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-result="you"
-if (($#>0)); then
-	result="$1"
-fi
-echo "One for $result, one for me."
