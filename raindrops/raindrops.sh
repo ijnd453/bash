f2038596baa2091d6b254d8e4b1ecba41347c0dc
ijnd453 <ijnd@umich.edu>
2023-05-19 15:41:51 -0400

    Completed Raindrops

--- raindrops/raindrops.sh
+++ raindrops/raindrops.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-
 output=""
-[ $(($1 % 3)) -eq 0 ] && output="${output}Pling"
-#[ $(($1 % 5)) -eq 0 ] && output="${output}Plang"
-#[ $(($1 % 7)) -eq 0 ] && output="${output}Plong"
-if [ output == "" ]; then
-	output="$1"
-fi
-echo "$output"
+[[ $(( $1 % 3 )) == 0 ]] && output+="Pling"
+[[ $(( $1 % 5 )) == 0 ]] && output+="Plang"
+[[ $(( $1 % 7 )) == 0 ]] && output+="Plong"
+[[ $output == "" ]] && output="${1}"
+echo "${output}"
--- upload.sh
+++ upload.sh
@@ -2,3 +2,5 @@
 git push https://github.com/ijnd453/bash.git
 ijnd453
 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
+
+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
