7b48e305f4d777fe5126e0b4789a417b53ea8683
ijnd453 <ijnd@umich.edu>
2023-06-06 00:02:28 -0400

    Wrote most of proverb

--- proverb/proverb.sh
+++ proverb/proverb.sh
@@ -1,24 +1,14 @@
 #!/usr/bin/env bash
-
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+proverb=""
+argPlusOne=0
+if [[ $# > 1 ]]; then
+	for (( arg = 1 ; arg < $# ; arg++))
+	do
+		argPlusOne=$(($arg + 1))
+		proverb+="For want of a ${!arg} the ${!argPlusOne} was lost. \n"
+	done
+fi
+if [[ $# > 0 ]]; then
+	proverb+="And all for the want of a $1.\n"
+fi
+echo -e $proverb
