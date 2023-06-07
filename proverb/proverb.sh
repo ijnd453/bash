3300c25130680298969edf205333f896bfa0136d
ijnd453 <ijnd@umich.edu>
2023-06-07 16:08:21 -0400

    Added acronym

--- proverb/proverb.sh
+++ proverb/proverb.sh
@@ -2,14 +2,14 @@
 set -o noglob
 proverb=""
 argPlusOne=0
-if [[ $# > 1 ]]; then
+if [[ $# -gt 1 ]]; then
 	for (( arg = 1 ; arg < $# ; arg++))
 	do
-		argPlusOne=$(($arg + 1))
+		argPlusOne=$((arg+1))
 		proverb+="For want of a ${!arg} the ${!argPlusOne} was lost.\n"
 	done
 fi
-if [[ $# > 0 ]]; then
+if [[ $# -gt 0 ]]; then
 	proverb+="And all for the want of a $1.\n"
 fi
-echo -e $proverb
+echo -e "$proverb"
