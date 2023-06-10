ba83aa1e3940dd07746bb7a44cc294d34d0a3702
ijnd453 <ijnd@umich.edu>
2023-06-09 20:58:22 -0400

    Completed acronym

--- acronym/acronym.sh
+++ acronym/acronym.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env bash
-
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+set -o noglob
+result_acronym=""
+if [[ $# -ne "1" ]]; then
+	echo "Usage: ./acronym.sh 'Input String'"
+	exit
+fi
+IFS=" -"
+for i in $1
+do
+	temp_i=$(echo "$i" | tr -dc '[:alpha:]')
+	temp_i="${temp_i:0:1}"
+	result_acronym+="${temp_i^^}"
+done
+echo $result_acronym | tr -dc '[:alnum:]\n\r'
