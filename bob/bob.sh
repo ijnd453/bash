7a52b2ff1f8fe3e43fc90b0fbe1ad1a2a9a21aa3
ijnd453 <ijnd@umich.edu>
2023-06-13 12:13:14 -0400

    First Iteration of bob.sh

--- bob/bob.sh
+++ bob/bob.sh
@@ -2,6 +2,8 @@
 set -o noglob
 q=0
 unspaced_input=$(tr -d [:space:] <<< "$1")
+only_letters_are_caps=0
+found_lower=0
 if [[ -z "$unspaced_input" ]]; then
 	echo "Fine. Be that way!"
 else
@@ -9,11 +11,28 @@ else
 	if [[ "${unspaced_input:(-1)}" == "?" ]]; then
 		q=1
 	fi
-	only_alpha=$(tr -cd [:alpha:] <<< "$unspaced_input")
+
+
+
+	for word in $(echo "$1"); do
+		len=${#word}
+		len_minus_one=$((len-1))
+		word_no_first="${word:1:len_minus_one}"
+		only_alpha=$(tr -cd [:alpha:] <<< "$word_no_first")
+		any_lower=$(tr -cd [:lower:] <<< "$only_alpha")
+		if [[ ! -z "$any_lower" ]]; then
+			found_lower=1
+		fi
+	done
+
+	[[ $found_lower -eq 0 ]] && only_letters_are_caps=1 || only_letters_are_caps=0
+	[[ -z $(tr -cd [:alpha:] <<< "$1") ]] && only_letters_are_caps=0
+
 	#echo "Q: $q"
-	#echo "Only_alpha: $only_alpha"
-	no_lower=$(tr -d [:lower:] <<< "$only_alpha")
-	if [[ -z "$no_lower" ]]; then
+	#echo "Found_lower: $found_lower"
+	#echo "only_letters_are_caps : $only_letters_are_caps"
+
+	if [[ $only_letters_are_caps -eq 0 ]]; then
 		[[ $q -eq 0 ]] && echo "Whatever." || echo "Sure."
 	else
 		[[ $q -eq 0 ]] && echo "Whoa, chill out!" || echo "Calm down, I know what I'm doing!"
