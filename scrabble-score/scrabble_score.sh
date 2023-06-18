39a689635b2479f2c208f77d101358ad2f34b258
ijnd453 <ijnd@umich.edu>
2023-06-18 17:57:06 -0400

    Completed scrabble score.sh

--- scrabble-score/scrabble_score.sh
+++ scrabble-score/scrabble_score.sh
@@ -1,24 +1,43 @@
 #!/usr/bin/env bash
+total=0
+input="$1"
+for ((i=0; i<${#input}; i++)); do
+	charac="${input:$i:1}"
+	upper="${charac^^}"
+	temp=0
+	case $upper in
+		"A" | "E" | "I" | "O" | "U" | "L" | "N" | "R" | "S" | "T" )
+			temp=1
+			;;
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+		"D" | "G" )
+			temp=2
+			;;
+
+		"B" | "C" | "M" | "P" )
+			temp=3
+			;;
+
+		"F" | "H" | "V" | "W" | "Y" )
+			temp=4
+			;;
+
+		"K" )
+			temp=5
+			;;
+
+		"J" | "X" )
+			temp=8
+			;;
+
+		"Q" | "Z" )
+			temp=10
+			;;
+
+		*)
+			temp=0
+			;;
+	esac
+	total=$((total+temp))
+done
+echo "$total"
